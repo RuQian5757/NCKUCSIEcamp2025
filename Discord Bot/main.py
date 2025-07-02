c156429e08c07c6da19e815fafc1309a7dde6ea3
SunnyShoun <chej40505@gmail.com>
2025-07-02 11:19:48 +0800

    fix bugs

--- Discord Bot/main.py
+++ Discord Bot/main.py
@@ -12,11 +12,12 @@ GUILD_ID = 000000000000000000000
 # 當機器人準備好時觸發
 @client.event
 async def on_ready():
+    await tree.sync(guild=discord.Object(id = GUILD_ID))  # 同步 Slash command
     print(f'Bot connected: {client.user.name}')
-    await tree.sync(guild=GUILD_ID)  # 同步 Slash command
 
 # 範例 Slash command: /hello
 @tree.command(name="hello", description="回應一個問候訊息")
+@app_commands.guilds(discord.Object(id = GUILD_ID))
 async def hello(interaction: discord.Interaction):
     await interaction.response.send_message(f'你好，{interaction.user.mention}！')
 
